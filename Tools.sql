7406ebc9deb7ae484fdd96d2204d95943f449e1d
GabrielMüllerDS <61920654+GabrielMullerDS@users.noreply.github.com>
2022-09-29 11:20:02 -0300

    Tools.sql são ferramentas para ajudar o back
    Arquivo de ferramentas para ajudar o back a executar algumas funções. Já implementado no banco de dados

---
+++ Tools.sql
@@ -0,0 +1,25 @@
+delimiter .
+# PAV = Pessoa, Aluno, Visitante
+create procedure insPAV(in nomePAV varchar(50),
+                        in emailPAV varchar(70),
+                        in telefonePAV varchar(14),
+                        in raPAV int(11),
+                        in cpfPAV char(11),
+                        in cursoPAV varchar(50),
+                        in periodoPAV int(11))
+begin
+  declare idPAV int;
+  insert into pessoa(nome, email, telefone)
+    values (nomePAV, emailPAV, telefonePAV);
+  if (raPAV = 0) then
+    set idPAV = last_insert_id();
+      insert into visitante(cpf, id_pessoa)
+        values (cpfPAV, idPAV);
+  else
+    set idPAV = last_insert_id();
+      insert into aluno(ra, id_pessoa, curso, semestre)
+        values(raPAV, idPAV, cursoPAV, periodoPAV);
+  end if;
+end .
+
+delimiter ;
