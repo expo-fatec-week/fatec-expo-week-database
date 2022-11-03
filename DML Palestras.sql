892d36883cb2444ce910b649ab8e67ecbe5f442b
Marcos Costa <marksous@gmail.com>
2022-11-02 21:24:22 -0300

    DML de Palestras e Projetos - Fase Final

---
+++ DML Palestras.sql
@@ -0,0 +1,14 @@
+insert into evento (descricao, tipo, data_evento) values ("SIMBOLOGIA DE SOLDAGEM", "palestra","2022-11-03 08:30:00");
+insert into evento (descricao, tipo, data_evento) values ("NANOTECNOLOGIA ", "palestra","2022-11-03 08:30:00");
+insert into evento (descricao, tipo, data_evento) values ("SAGA GAMES", "palestra","2022-11-03 14:30:00");
+insert into evento (descricao, tipo, data_evento) values ("ROBÓTICA COLABORATIVA NA INDÚSTRIA 4.0", "palestra","2022-11-03 19:30:00");
+insert into evento (descricao, tipo, data_evento) values ("GLOBALIZAÇÃO, INOVAÇÃO OU REVOLUÇÃO?", "palestra","2022-11-03 19:30:00");
+insert into evento (descricao, tipo, data_evento) values ("SEJA CEO DA SUA PRÓPRIA VIDA", "palestra","2022-11-03 19:30:00");
+insert into evento (descricao, tipo, data_evento) values ("COMO CONSEGUIR TER SUCESSO E TRAZER RESULTADOS EM QUALQUER ÁREA DA SUA VIDA.", "palestra","2022-11-03 19:30:00");
+insert into evento (descricao, tipo, data_evento) values ("ROBÓTICA COLABORATIVA NA INDÚSTRIA 4.0 / MANUFATURA ADITIVA, AUTOMAÇÃO E STRATEGIC ANALYTICS: BIG DATA E CYBER SEGURANÇA.", "palestra","2022-11-04 08:30:00");
+insert into evento (descricao, tipo, data_evento) values (" AUTOMAÇÃO E CONTROLE – ECONOMIA E SUSTENTABILIDADE NA REFRIGERAÇÃO", "palestra","2022-11-04 08:30:00");
+insert into evento (descricao, tipo, data_evento) values ("CONTABILIDADE PARA NÃO CONTADOR", "palestra","2022-11-04 14:30:00");
+insert into evento (descricao, tipo, data_evento) values ("PROBLEMAS EM REDES. CASOS PRÁTICOS.", "palestra","2022-11-04 19:30:00");
+insert into evento (descricao, tipo, data_evento) values ("TECNOLOGIA DE DRONE / APLICAÇÕES DE DRONE", "palestra","2022-11-04 19:30:00");
+insert into evento (descricao, tipo, data_evento) values ("COMO PASSAR EM QUALQUER PROCESSO SELETIVO E TER UMA VIDA FINANCEIRA FELIZ ", "palestra","2022-11-04 19:30:00");
+insert into evento (descricao, tipo, data_evento) values ("INTELIGÊNCIA EMOCIONAL", "palestra","2022-11-04 19:30:00");
