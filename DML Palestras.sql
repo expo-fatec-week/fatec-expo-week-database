444717289b694ccdf773507b0b89b10ba1478c8d
Wesley Batista <wesley@bestpontos.com.br>
2023-05-24 15:58:15 -0300

    Palestras do segundo semestre de 2023

--- DML Palestras.sql
+++ DML Palestras.sql
@@ -12,3 +12,18 @@ insert into evento (descricao, tipo, data_evento) values ("PROBLEMAS EM REDES. C
 insert into evento (descricao, tipo, data_evento) values ("TECNOLOGIA DE DRONE / APLICAÇÕES DE DRONE", "PALESTRA","2023-04-18 19:30:00");
 insert into evento (descricao, tipo, data_evento) values ("COMO PASSAR EM QUALQUER PROCESSO SELETIVO E TER UMA VIDA FINANCEIRA FELIZ ", "PALESTRA","2023-04-18 19:30:00");
 insert into evento (descricao, tipo, data_evento) values ("INTELIGÊNCIA EMOCIONAL", "PALESTRA","2023-04-18 19:30:00");
+
+-- Segundo semestre de 2023:
+
+insert into evento (descricao, tipo, local, data_evento) values ("SISTEMAS DE GESTÃO DE ENERGIA (ISO 55001) E EFICIÊNCIA HÍDRICA (ISO 46001) - BENEFÍCIOS E DESAFIOS PARA O SETOR INDUSTRIAL", "PALESTRA", "BIBLIOTECA", "2023-05-25 08:30:00");
+insert into evento (descricao, tipo, local, data_evento) values ("CLOUDCOMPUTING - COM CERTIFICAÇÃO", "PALESTRA", "BIBLIOTECA", "2023-05-25 14:30:00");
+insert into evento (descricao, tipo, local, data_evento) values ("MANUTENÇÃO PREDITIVA NA INDUSTRIA 4.0", "PALESTRA", "AUDITÓRIO", "2023-05-25 19:30:00");
+insert into evento (descricao, tipo, local, data_evento) values ("SEGREDOS DO PREÇO LUCRATIVO", "PALESTRA", "BIBLIOTECA", "2023-05-25 19:30:00");
+insert into evento (descricao, tipo, local, data_evento) values ("DESVENDANDO AS NUVENS: EXPLORANDO AS VANTAGENS E DESAFIOS DA COMPUTAÇÃO EM NUVEM PÚBLICA, PRIVADA E HÍBRIDA.", "PALESTRA", "SALA B221", "2023-05-25 19:30:00");
+insert into evento (descricao, tipo, local, data_evento) values ("CONVERGÊNCIA TECNOLÓGICA NA ERA DO DIAGNÓSTICO: O PAPEL DOS PROFISSIONAIS MULTIDISCIPLINARES", "PALESTRA", "BIBLIOTECA", "2023-05-26 08:30:00");
+insert into evento (descricao, tipo, local, data_evento) values ("EFICIENCIA ENERGETICA EM SISTEMAS DE AR COMPRIMIDO", "PALESTRA", "SALA B221", "2023-05-26 08:30:00");
+insert into evento (descricao, tipo, local, data_evento) values ("APAIXONE- SE PELO PROBLEMA.", "PALESTRA", "BIBLIOTECA", "2023-05-26 13:50:00");
+insert into evento (descricao, tipo, local, data_evento) values ("CENÁRIO DO RAMO DE SEGUROS: COLOCAÇÃO PROFISSIONAL, ANÁLISE  DO MERCADO E EDUCAÇÃO CONTINAUDA", "PALESTRA", "BIBLIOTECA", "2023-05-26 15:50:00");
+insert into evento (descricao, tipo, local, data_evento) values ("DATA DRIVEN SERVICES - ALTURE", "PALESTRA", "AUDITÓRIO", "2023-05-26 19:30:00");
+insert into evento (descricao, tipo, local, data_evento) values ("OPORTUNIDADES E DESAFIOS DO SETOR DE TI", "PALESTRA", "BIBLIOTECA", "2023-05-26 19:30:00");
+insert into evento (descricao, tipo, local, data_evento) values ("SAÚDE MENTAL / BRASIL, O PAÍS DO FUTURO?", "PALESTRA", "SALA B221", "2023-05-26 19:30:00");
